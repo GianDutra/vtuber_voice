624c6912a5661fbf25ee7aebb4c43635a33295db
GianDutra <chikegang@gmail.com>
2023-03-08 14:05:07 -0300

    Versao alpha 1.2

--- import time.py
+++ import time.py
@@ -2,16 +2,33 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+from googletrans import Translator
 import pygame
 import time
 import keyboard
 import speech_recognition as sr
 
 
-
-driver = webdriver.Chrome()
 pygame.init()
 pygame.mixer.music.load("anya.ogg")
+programa_rodando = True
+
+
+# cria uma instância do objeto Translator
+translator = Translator()
+
+# Define o idioma padrão do aplicativo 
+global idioma
+idioma = "pt-br"
+
+# Deixa a traducao ativada
+global traducao
+traducao = True
+
+# Define o tamanho do audio
+global tam_audio
+tam_audio = 4
 
 opt = Options()
 opt.add_argument("--disable-infobars")
@@ -32,9 +49,11 @@ driver = webdriver.Chrome(chrome_options=opt, executable_path=r'C:\Python39\chro
 driver.get('https://voicevox.su-shiki.com/#try')
 
 
+
+
 def gravarAudio():
 
-    
+    global traducao
     # Executa a música previamente carregada
     pygame.mixer.music.play()
 
@@ -42,24 +61,49 @@ def gravarAudio():
     r = sr.Recognizer()
 
     # Define o tempo máximo de silêncio para 2 segundos
-    silence_time = 5
+    silence_time = tam_audio
 
 
     # Usa o microfone como fonte de áudio
     with sr.Microphone() as source:
-        print("Fale algo...")
-        audio = r.listen(source, phrase_time_limit=silence_time)
+        
 
-        try:
-            # Transcreve o áudio em texto
-            texto = r.recognize_google(audio, language='ja-JP')
-            print("Você disse: ", texto)
-        except sr.UnknownValueError:
-            print("Não entendi o que você disse")
-        except sr.RequestError as e:
-            print("Não foi possível conectar-se ao serviço de reconhecimento de fala; {0}".format(e))
+        
 
-    return texto
+        if traducao : 
+            try:
+                print("Fale algo...")
+                audio = r.listen(source, phrase_time_limit=silence_time)   
+                # Transcreve o áudio em texto
+                texto_em_portugues = r.recognize_google(audio, language=idioma)
+
+                if texto_em_portugues.startswith('Como') or texto_em_portugues.startswith('O que') or texto_em_portugues.startswith('Qual'):
+                 texto_em_portugues += '?'
+
+                print("Você disse(com traducao): ", texto_em_portugues)
+                
+                texto = translator.translate(texto_em_portugues, dest='ja')
+                print("A traducao e: ", texto.text)
+                
+                texto = texto.text
+            except sr.UnknownValueError:
+                print("Não entendi o que você disse")
+            except sr.RequestError as e:
+                print("Não foi possível conectar-se ao serviço de reconhecimento de fala; {0}".format(e))
+                
+        else:
+            try:
+                print("Fale algo...")
+                audio = r.listen(source, phrase_time_limit=silence_time)
+                # Transcreve o áudio em texto
+                texto = r.recognize_google(audio, language='ja-JP')
+                print("Você disse(sem traducao): ", texto)
+            except sr.UnknownValueError:
+                print("Não entendi o que você disse")
+            except sr.RequestError as e:
+                print("Não foi possível conectar-se ao serviço de reconhecimento de fala; {0}".format(e))
+
+        return texto
 
 
 def toVtuber(texto):
@@ -77,16 +121,94 @@ def callback_function():
         toVtuber(texto)
 
 
-def finalizar_programa():
+def parar_programa():
+    global programa_rodando
+    programa_rodando = False
     pygame.quit()
+    keyboard.unhook_all_hotkeys()
+    keyboard.clear_all_hotkeys()
     driver.quit()
-    exit()
+
+def ingles():
+    global idioma
+    idioma = "en-US"
+    print("Idioma atualizado para inglês")
+
+def portugues():
+    global idioma
+    idioma = "pt-BR"
+    print("Idioma atualizado para português")
+
+def espanhol():
+    global idioma
+    idioma = "es-ES"
+    print("Idioma atualizado para espanhol")
+
+
+def traducao_desativada():
+    global traducao
+    if traducao:
+        traducao = False
+        print("Tradução desativada")
+    else:
+        traducao = True
+        print("Tradução ativada")
+
+def aumenta_tamanho_audio():
+    global tam_audio
+    tam_audio = tam_audio + 1
+    print("O tamanho do áudio agora é:" + str(tam_audio))
+
+def diminui_tamanho_audio():
+    global tam_audio
+    tam_audio = tam_audio - 1
+    print("O tamanho do áudio agora é:" + str(tam_audio))
+
+def mudarVozParaMochiko():
+    combo_box = driver.find_element(By.XPATH, '//*[@id="charSelect"]')
+    # selecione a opção com o valor "10"
+    select = Select(combo_box)
+    select.select_by_value("10")
+    print("Sua voz agora é de uma mulher")
+
+def mudarVozParaHomem():
+    combo_box = driver.find_element(By.XPATH, '//*[@id="charSelect"]')
+    # selecione a opção com o valor "21"
+    select = Select(combo_box)
+    select.select_by_value("21")
+    print("Sua voz agora é de um homem")
+
+def mudarVozParaDefault():
+    combo_box = driver.find_element(By.XPATH, '//*[@id="charSelect"]')
+    # selecione a opção com o valor "21"
+    select = Select(combo_box)
+    select.select_by_value("1")
+    print("Sua voz agora é de uma menina")
 
 
 keyboard.add_hotkey('ctrl+alt+a', callback_function)
 
-keyboard.add_hotkey('ctrl+alt+p', finalizar_programa)
+keyboard.add_hotkey('ctrl+alt+h', parar_programa)
 
+keyboard.add_hotkey('ctrl+3', portugues)
 
-keyboard.wait()
+keyboard.add_hotkey('ctrl+4', espanhol)
+
+keyboard.add_hotkey('ctrl+5', ingles)
+
+keyboard.add_hotkey('ctrl+6', traducao_desativada)
+
+keyboard.add_hotkey('ctrl+alt+o', diminui_tamanho_audio)
+
+keyboard.add_hotkey('ctrl+alt+p', aumenta_tamanho_audio)
+
+keyboard.add_hotkey('ctrl+alt+k', mudarVozParaMochiko)
+
+keyboard.add_hotkey('ctrl+alt+l', mudarVozParaHomem)
+
+keyboard.add_hotkey('ctrl+alt+j', mudarVozParaDefault)
+
+while programa_rodando:
+    keyboard.wait()
+        
 
